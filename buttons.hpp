e5aa3947531d3d6fd84e2d875f18eb56fd99da3b
Knechti <156342197+KnechtUnrecht@users.noreply.github.com>
2025-03-04 19:28:27 +0100

    Counter-Strike 2 Offsets
    Uploaded on 04/03/2025

---
+++ buttons.hpp
@@ -0,0 +1,28 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-03-04 18:22:40.049760800 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x1883720;
+        constexpr std::ptrdiff_t attack2 = 0x18837B0;
+        constexpr std::ptrdiff_t back = 0x18839F0;
+        constexpr std::ptrdiff_t duck = 0x1883CC0;
+        constexpr std::ptrdiff_t forward = 0x1883960;
+        constexpr std::ptrdiff_t jump = 0x1883C30;
+        constexpr std::ptrdiff_t left = 0x1883A80;
+        constexpr std::ptrdiff_t lookatweapon = 0x1AAC5C0;
+        constexpr std::ptrdiff_t reload = 0x1883690;
+        constexpr std::ptrdiff_t right = 0x1883B10;
+        constexpr std::ptrdiff_t showscores = 0x1AAC4A0;
+        constexpr std::ptrdiff_t sprint = 0x1883600;
+        constexpr std::ptrdiff_t turnleft = 0x1883840;
+        constexpr std::ptrdiff_t turnright = 0x18838D0;
+        constexpr std::ptrdiff_t use = 0x1883BA0;
+        constexpr std::ptrdiff_t zoom = 0x1AAC530;
+    }
+}
